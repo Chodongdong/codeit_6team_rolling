ea7dec95f6eeb4acd3e77e0145b28ba24f9518e6
apple <apple@apples-MacBook-Air.local>
2025-09-26 23:27:57 +0900

    공용컴포넌트구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
-import "./App.css";
+import React from "react";
+import Input from "./Components/Input/Input";
+import ArrowButton from "./Components/ArrowButton/ArrowButton";
 
-function App() {
-  return <></>;
-}
+const App: React.FC = () => {
+  const handleLeftClick = () => {
+    console.log("왼쪽 버튼 클릭");
+  };
+
+  const handleRightClick = () => {
+    console.log("오른쪽 버튼 클릭");
+  };
+
+  return (
+    <div>
+      {/* 일반 인풋 */}
+      <Input placeholder="Placeholder" />
+
+      {/* 비활성화 인풋 */}
+      <Input placeholder="Placeholder" disabled />
+
+      {/* 화살표 버튼 */}
+      <div>
+        <ArrowButton direction="left" onClick={handleLeftClick} />
+        <ArrowButton direction="right" onClick={handleRightClick} />
+      </div>
+    </div>
+  );
+};
 
 export default App;
