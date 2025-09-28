6b2f8f224da70e7aae4140856b8f9a6f3789fe1f
k0nghaa <k0nghaalee@gmail.com>
2025-09-28 23:28:57 +0900

    test: App.tsx에 공용 컴포넌트 임시 추가 (렌더링 확인용)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import "./App.css";
+import './App.css';
+import Badge from './components/common/Badge';
+import BadgeEmoji from './components/common/BadgeEmoji';
+import ToggleButton from './components/common/ToggleButton';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Badge variant="other" className="is-selected" />
+      <Badge variant="friend" />
+      <Badge variant="coworker" />
+      <Badge variant="family" />
+      <BadgeEmoji />
+      <ToggleButton />
+    </>
+  );
 }
 
 export default App;
