e27ec1eabe1952b749448cfb9a993eef488fa2b5
k0nghaa <k0nghaalee@gmail.com>
2025-09-28 23:25:41 +0900

    design: ToggleButton 정적 UI 구현
    기본 디자인

---
+++ src/components/common/ToggleButton.tsx
@@ -0,0 +1,12 @@
+import './ToggleButton.css';
+
+const ToggleButton = () => {
+  return (
+    <div className="ToggleButton">
+      <div className="color">컬러</div>
+      <div className="image">이미지</div>
+    </div>
+  );
+};
+
+export default ToggleButton;
