8034d54d59bed62fe2e252ce9bdc63d9b1e6c51e
k0nghaa <k0nghaalee@gmail.com>
2025-09-29 15:29:57 +0900

    refactor: BadgeEmoji 컴포넌트 리팩터링 (범용성 개선, 주석 추가)

--- src/components/common/BadgeEmoji.tsx
+++ src/components/common/BadgeEmoji.tsx
@@ -1,19 +1,29 @@
-import { useState } from 'react';
+/**
+ * 공감 이모지와 해당 이모지에 대한 카운트(숫자)를 표시하는 UI 컴포넌트
+ *
+ * @component
+ * @example
+ * // 기본 사용 예시 - 상위 컴포넌트에서 props로 전달받아 출력
+ * <BadgeEmoji emoji="🥳" count={14} />
+ *
+ * @param {string} emoji - 표시할 이모지 (문자열)
+ * @param {number} count - 해당 이모지의 카운트 값
+ * @returns {JSX.Element} 이모지와 카운트를 함께 출력하는 JSX 엘리먼트
+ */
+
 import './BadgeEmoji.css';
 
-const BadgeEmoji = () => {
-  // const [emoji, setEmoji] = useState('😇');
-  const [count, setCount] = useState(0);
+interface BadgeEmojiProps {
+  emoji: string;
+  count: number;
+}
 
-  const handleClick = () => {
-    setCount((prev) => prev + 1);
-  };
-
+const BadgeEmoji: React.FC<BadgeEmojiProps> = ({ emoji, count }) => {
   return (
-    <button className="BadgeEmoji" onClick={handleClick}>
-      <div className="BadgeEmoji_emoji">😇</div>
-      <div className="BadgeEmoji_number">{count}</div>
-    </button>
+    <div className="BadgeEmoji">
+      {emoji && <span>{emoji}</span>}
+      <span>{count}</span>
+    </div>
   );
 };
 
