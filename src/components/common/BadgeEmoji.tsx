4bac461b6363f3bd06d6e25b9c73b089d6c3abab
k0nghaa <k0nghaalee@gmail.com>
2025-09-30 08:44:35 +0900

    refactor: BadgeEmoji onClick 외부 주입 가능하도록 개선
    div -> button 수정

--- src/components/common/BadgeEmoji.tsx
+++ src/components/common/BadgeEmoji.tsx
@@ -12,9 +12,11 @@
  *
  * @param {EmojiKey} [emoji] - 표시할 이모지의 key (예: 'party', 'love')
  * @param {number} count - 해당 이모지의 카운트 값
+ * @param {(event: React.MouseEvent<HTMLButtonElement>) => void} [onClick] - 클릭 이벤트 핸들러
  * @returns {JSX.Element} 이모지와 카운트를 함께 출력하는 JSX 엘리먼트
  */
 
+import React from 'react';
 import './BadgeEmoji.css';
 
 // 이모지 모음 객체(추후 추가 가능)
@@ -31,15 +33,20 @@ type EmojiKey = keyof typeof emojiMap;
 interface BadgeEmojiProps {
   emoji?: EmojiKey; // 선택적: 없으면 숫자만 출력
   count: number; // 필수: 무조건 출력
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void; // 선택적: 외부 주입
 }
 
-const BadgeEmoji: React.FC<BadgeEmojiProps> = ({ emoji, count }) => {
-  return (
-    <div className="BadgeEmoji">
-      {emoji && <span>{emojiMap[emoji]}</span>}
-      <span>{count}</span>
-    </div>
-  );
-};
+const BadgeEmoji = React.forwardRef<HTMLButtonElement, BadgeEmojiProps>(
+  ({ emoji, count, onClick }, ref) => {
+    return (
+      <button ref={ref} className="BadgeEmoji" onClick={onClick}>
+        {emoji && <span>{emojiMap[emoji]}</span>}
+        <span>{count}</span>
+      </button>
+    );
+  }
+);
+
+BadgeEmoji.displayName = 'BadgeEmoji'; //디버깅 위해 작성한 코드(없어도 동작함)
 
 export default BadgeEmoji;
