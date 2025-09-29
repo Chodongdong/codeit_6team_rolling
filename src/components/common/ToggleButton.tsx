de0cf491a928ef60eca1992211b699fa67f3f4ff
k0nghaa <k0nghaalee@gmail.com>
2025-09-30 08:31:32 +0900

    docs: ToggleButton 컴포넌트 주석 추가

--- src/components/common/ToggleButton.tsx
+++ src/components/common/ToggleButton.tsx
@@ -1,3 +1,32 @@
+/**
+ * 두 가지 옵션 중 하나를 선택할 수 있는 토글 버튼 UI.
+ *
+ *
+ * @component
+ * @example
+ * // App.tsx
+ * import ToggleButton from './ToggleButton';
+ *
+ * function App() {
+ *    //상위 컴포넌트에서 원하는 옵션을 지정할 수 있음.
+ *   const [value, setValue] = useState<'Light' | 'Dark'>('Light');
+ *
+ *   return (
+ *     <ToggleButton
+ *       options={['Light', 'Dark']}
+ *       value={value}
+ *       onValueChange={setValue}
+ *     />
+ *   );
+ * }
+ *
+ * @prop {string[]} options - 두 가지 옵션을 담은 배열 (ex: ['컬러', '이미지'])
+ * @prop {string} value - 현재 선택된 옵션 값
+ * @prop {(value: string) => void} onValueChange - 선택된 옵션이 변경될 때 실행되는 핸들러
+ * @prop {string} [className] - 커스텀 스타일 적용을 위한 선택적 클래스명
+ * @returns {JSX.Element} 토글 버튼 UI
+ */
+
 import './ToggleButton.css';
 import type { ComponentPropsWithoutRef } from 'react';
 
