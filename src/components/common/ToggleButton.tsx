2050ebead034c66762dfd58dbc0ec0d9fe6f7c41
k0nghaa <k0nghaalee@gmail.com>
2025-09-29 10:30:01 +0900

    feat: 토글버튼 슬라이드 애니메이션 추가
    컬러 이미지 토글버튼

--- src/components/common/ToggleButton.tsx
+++ src/components/common/ToggleButton.tsx
@@ -1,10 +1,25 @@
+import { useState } from 'react';
 import './ToggleButton.css';
 
 const ToggleButton = () => {
+  const [selected, setSelected] = useState<'color' | 'image'>('color');
+
   return (
     <div className="ToggleButton">
-      <div className="color">컬러</div>
-      <div className="image">이미지</div>
+      <div className={`slider ${selected === 'color' ? 'left' : 'right'}`} />
+
+      <div
+        className={`option ${selected === 'color' ? 'active' : ''}`}
+        onClick={() => setSelected('color')}
+      >
+        컬러
+      </div>
+      <div
+        className={`option ${selected === 'image' ? 'active' : ''}`}
+        onClick={() => setSelected('image')}
+      >
+        이미지
+      </div>
     </div>
   );
 };
