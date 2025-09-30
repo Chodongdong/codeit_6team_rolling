3c4027d1aa383aa18b0f8ac30f250d232504fa52
k0nghaa <k0nghaalee@gmail.com>
2025-09-30 09:39:30 +0900

    refactor: Badge CSS에서 불필요한 is-selected 제거 및 스타일 통합

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,16 @@ import ToggleButton from './components/common/ToggleButton';
 
 function App() {
   const [toggleValue, setToggleValue] = useState('컬러');
+  const [clickedBadge, setClickedBadge] = useState<string>('');
+
   return (
     <>
-      <Badge variant="other" className="is-selected" />
-      <Badge variant="friend" />
-      <Badge variant="coworker" />
+      <Badge variant="other" onClick={() => setClickedBadge('other')} />
+      <Badge variant="friend" onClick={() => setClickedBadge('friend')} />
+      <Badge variant="coworker" onClick={() => setClickedBadge('coworker')} />
       <Badge variant="family" />
+      {clickedBadge && <p>마지막으로 클릭한 뱃지: {clickedBadge}</p>}
+
       <BadgeEmoji emoji="party" count={14} />
       <div>
         <h3>선택된 값: {toggleValue}</h3>
