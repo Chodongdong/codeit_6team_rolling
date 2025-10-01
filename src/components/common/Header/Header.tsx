3067199f3183f39c44fb2f028ba85e5ff04a4d01
k0nghaa <k0nghaalee@gmail.com>
2025-10-01 09:58:58 +0900

    design: Header 정적 UI 구조 및 스타일 구현
    - arrow_down icon 추가

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,5 +1,6 @@
 import "./Header.css";
 import Button from "../buttons/button";
+import BadgeEmoji from "../BadgeEmoji/BadgeEmoji";
 
 const Header = () => {
   return (
@@ -13,7 +14,32 @@ const Header = () => {
           children="롤링 페이퍼 만들기"
         />
       </div>
-      <div className="service"></div>
+      <div className="service">
+        <div className="service__recipient">To. 수취인</div>
+        <div className="service__meta">
+          <img className="service__avatars" />
+          <div className="service__count">
+            N<span>명이 작성했어요!</span>
+          </div>
+          <div className="service__divider">|</div>
+
+          <div className="service__reactions">
+            <BadgeEmoji count={0} />
+            <BadgeEmoji count={0} />
+            <BadgeEmoji count={0} />
+            <img
+              className="service__reaction-arrow"
+              src="src/assets/ic/arrow_down.svg"
+            />
+          </div>
+
+          <div className="service__actions">
+            <Button variant="outlined" size="md" shape="icon" children="추가" />
+            <div className="service__divider">|</div>
+            <Button variant="outlined" size="md" shape="icon" />
+          </div>
+        </div>
+      </div>
     </header>
   );
 };
