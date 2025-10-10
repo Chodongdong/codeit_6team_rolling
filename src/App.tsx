cadba7c45bc85f23f49a031bef0320e2001dfb94
k0nghaa <k0nghaalee@gmail.com>
2025-10-11 00:06:37 +0900

    feat: Layout 및 Routes 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
 // src/App.tsx
-import React from 'react';
-import MainPage from './pages/MainPage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/common/Layout/Layout";
+import MainPage from "./pages/MainPage";
+import RouteTest from "./pages/RouteTest";
 
-const App: React.FC = () => {
-  return <MainPage />;
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainPage />} /> {/* 메인 페이지 */}
+          <Route path="list" element={<RouteTest />} />{" "}
+          {/* 롤링페이퍼 목록 페이지 */}
+          <Route path="post/:id" element={<RouteTest />} />{" "}
+          {/* 생성된 롤링페이퍼 페이지 */}
+          <Route path="post" element={<RouteTest />} /> {/* 생성 페이지 */}
+          <Route path="post/:id/message" element={<RouteTest />} />{" "}
+          {/* 롤링페이퍼에 메시지 보내기 페이지 */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
