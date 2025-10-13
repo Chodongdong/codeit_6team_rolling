3ca74d5d336a4dae25a61292fd3c61ebdd137550
오상우 <sangwoo2512@gmail.com>
2025-10-14 08:51:31 +0900

    chore(app): 라우트/엔트리 정리/메인페이지

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 // src/App.tsx
-import React from "react";
+import { Routes, Route, Navigate } from 'react-router-dom';
 
-import MainPages from "./pages/MainPages";
+import MainHome from './pages/MainHome';
+import List from './pages/List';     
+
+import Post from './pages/post';
 
 const App: React.FC = () => {
-  return <MainPages />;
+  return (
+    <Routes>
+      {/* 메인 랜딩 */}
+      <Route path="/" element={<MainHome />} />
+
+      <Route path="/list" element={<List />} />
+
+      <Route path="/post" element={<Post />} />
+
+      {/* 404 */}
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  );
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,22 @@
+// src/main.tsx
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
-// 1) 리셋을 최우선으로
 import './styles/reset.css';
-
-// 2) 전역 스타일(CDN 포함)
 import './index.css';
 
 import App from './App.tsx';
-
-// 토스트 전역 연결
 import { ToastProvider } from './components/Toast/useToast';
 import ToastContainer from './components/Toast/ToastContainer';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ToastProvider>
-      <App />
-      <ToastContainer />
+      <BrowserRouter>
+        <App />
+        <ToastContainer />
+      </BrowserRouter>
     </ToastProvider>
   </StrictMode>
 );
